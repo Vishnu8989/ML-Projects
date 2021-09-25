f9e534c7291180d245bf5b574047248a7224c37a
Vishnu <vishnusingh1057@gmail.com>
2021-09-25 10:32:40 +0530

    Uploaded Via Upload

---
+++ Cats And Dogs/NN.py
@@ -0,0 +1,126 @@
+from keras.preprocessing import image
+import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop
+import tensorflow as tf
+import os
+import zipfile
+
+local_zip = './cats_and_dogs_filtered.zip'
+
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+
+zip_ref.extractall('./')
+zip_ref.close()
+
+base_dir = './cats_and_dogs_filtered'
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training cat/dog pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+# Directory with our validation cat/dog pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+train_cat_fnames = os.listdir(train_cats_dir)
+train_dog_fnames = os.listdir(train_dogs_dir)
+
+print(train_cat_fnames[:10])
+print(train_dog_fnames[:10])
+
+print('total training cat images :', len(os.listdir(train_cats_dir)))
+print('total training dog images :', len(os.listdir(train_dogs_dir)))
+
+print('total validation cat images :', len(os.listdir(validation_cats_dir)))
+print('total validation dog images :', len(os.listdir(validation_dogs_dir)))
+
+model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 150x150 with 3 bytes color
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu',
+                           input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('cats') and 1 for the other ('dogs')
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+
+model.compile(optimizer=RMSprop(learning_rate=0.001),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+
+# All images will be rescaled by 1./255.
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+test_datagen = ImageDataGenerator(rescale=1./255,
+                                  rotation_range=40,
+                                  width_shift_range=0.2,
+                                  height_shift_range=0.2,
+                                  shear_range=0.2,
+                                  zoom_range=0.2,
+                                  horizontal_flip=True,
+                                  fill_mode='nearest')
+
+# --------------------
+# Flow training images in batches of 20 using train_datagen generator
+# --------------------
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    batch_size=20,
+                                                    class_mode='binary',
+                                                    target_size=(150, 150))
+# --------------------
+# Flow validation images in batches of 20 using test_datagen generator
+# --------------------
+validation_generator = test_datagen.flow_from_directory(validation_dir,
+                                                        batch_size=20,
+                                                        class_mode='binary',
+                                                        target_size=(150, 150))
+
+history = model.fit(train_generator,
+                    validation_data=validation_generator,
+                    steps_per_epoch=100,
+                    epochs=10,
+                    validation_steps=50,
+                    verbose=2)
+
+
+
+
+
+    # predicting images
+path = './d2.jpg'
+img = image.load_img(path, target_size=(150, 150))
+
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+images = np.vstack([x])
+
+classes = model.predict(images, batch_size=10)
+
+print(classes[0])
+
+if classes[0] > 0:
+    print("Picture is of a dog")
+
+else:
+    print("Picture is of a cat")
